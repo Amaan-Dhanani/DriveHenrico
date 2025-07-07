e7f8850d1e7552b80a2be4fba15100b3ab6fecae
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-07-07 18:37:58 -0300

    setup(frontend): Bare frontend setup

---
+++ packages/frontend/android/app/src/main/java/com/drivehenrico/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.drivehenrico.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ packages/frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.drivehenrico.app',
+  appName: 'DriveHenrico',
+  webDir: 'dist'
+};
+
+export default config;
