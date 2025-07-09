e73b4c3d223282c1d7d603c6a621e43abedfad89
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-07-09 00:35:24 -0300

    fix(ConfigLoading): Add gathering of predetermined initially

--- packages/backend/main.py
+++ packages/backend/main.py
@@ -4,8 +4,11 @@ sys.dont_write_bytecode = True
 
 import uvicorn
 from utils.app.Quart import app
+from siblink import Config
+
 
 # Load Everything
+Config.gather_predetermined()
 app.register_blueprints()
 
 uvicorn.run(app, **app.uvicorn_config)
