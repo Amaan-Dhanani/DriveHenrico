3f5d6a1c2410d05c026d22a021bb20efe1e54920
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-07-09 22:39:59 -0300

    chore(config): Ensure file exists

--- packages/backend/main.py
+++ packages/backend/main.py
@@ -5,11 +5,19 @@ sys.dont_write_bytecode = True
 import uvicorn
 from utils.app.Quart import app
 from siblink import Config
+from utils.helper.config import Yaml
+
+
+# Ensure Config
+try:
+    Yaml()
+except FileNotFoundError:
+    print("Failed to run, no config file specified was found.\n You should copy config.example.yml into config.yml and try again.")
+    quit()
 
 
 # Load Everything
 Config.gather_predetermined()
 app.register_blueprints()
 
-uvicorn.run(app, **app.uvicorn_config)
-
+uvicorn.run(app, **Yaml().get("backend.uvicorn_config"))
