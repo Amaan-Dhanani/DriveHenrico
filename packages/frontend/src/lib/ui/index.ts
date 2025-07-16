3ef58aad508909d917b06bfdaf610122a7ac0103
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-07-15 20:03:27 -0400

    remove(table comp, not needed, it caused errors), vip(settings page)

--- packages/frontend/src/lib/ui/index.ts
+++ packages/frontend/src/lib/ui/index.ts
@@ -1,3 +1,2 @@
 export { default as Header } from './header/components/header.svelte';
 export { default as Text } from './text/components/text.svelte';
-export * as Table from '../ui/table';
--- packages/frontend/src/routes/settings/+page.svelte
+++ packages/frontend/src/routes/settings/+page.svelte
@@ -17,20 +17,21 @@
 	<!-- Header -->
 	<Header xxl bold>Get Started</Header>
 	<Text lg class="opacity-80">Enter your details below to start your journey!</Text>
-
 	<!-- Form Section -->
 	<Frame flex col fill class="mt-2 box-border rounded-t-2xl p-6 dark:bg-[#2F2F42]">
-		<form class="flex flex-row items-end gap-4 rounded-t-xl bg-white p-6 pb-0 dark:bg-[#2F2F42]">
+			TO BE DISPLAYED, IF YOU ARE LOGGED IN
+		<form class="flex flex-row items-end gap-4 rounded-t-xl bg-white px-6 dark:bg-[#2F2F42]">
 			<div class="flex-1">
 				<Input type="text" id="name_input" name="name" label="Name" value="NAME IN BATABASE" />
 			</div>
-			<Button>Update</Button>
+			<Button class="text-white">Update</Button>
 		</form>
 		<form class="flex flex-row items-end gap-4 bg-white p-6 pb-0 dark:bg-[#2F2F42]">
 			<div class="flex-1">
 				<Input type="email" id="email_input" name="email" label="Email" value="EMAIL IN DATABASE" />
 			</div>
-			<Button>Update</Button>
+			<Button class="text-white">Update</Button>
 		</form>
+			TO BE DISPLAYED WHENEVER
 	</Frame>
 </Flex>
