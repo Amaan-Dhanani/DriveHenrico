34d8b555b299b94e066c1d4c3bc456323a3b9076
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-07-19 21:55:45 -0400

    chore(icons): add npm package with setup; feat(light_dark): started component

---
+++ packages/frontend/src/lib/components/lightdark/LightDark.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Dark from '~icons/ic/round-dark-mode';
+    import Light from '~icons/ic/round-light-mode';
+    import { Flex } from 'sk-clib';
+    import { Text } from '@lib/ui';
+    export let type = 'link';
+
+</script>
+
+<Flex>
+    <Dark class="w-6 h-6"/>
+    <Text class="text-sm">Dark</Text>
+</Flex>
+
+<!-- <Flex>
+    <Light class="w-6 h-6"/>
+    <Text class="text-sm">Light</Text>
+</Flex> -->
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
+	plugins: [sveltekit(), tailwindcss(), Icons({ autoInstall: true, compiler: 'svelte' })],
 	server: {
 		allowedHosts: true
 	}
