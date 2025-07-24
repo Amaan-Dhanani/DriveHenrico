906a33ace074dce7c92a59a1dd746bff27d794b3
Trelta Sebastion (home:linux) <treltasev@gmail.com>
2025-07-23 21:37:23 -0400

    fix(radio-horizontal): Fixed weird bug

--- packages/frontend/src/lib/components/RadioHorizontal/index.ts
+++
@@ -1,7 +0,0 @@
-export {default as Menu} from "./components/radiohorizontal-menu.svelte";
-
-export { default as Button } from "./components/radiohorizontal-button.svelte";
-export type {
-    tRadioHorizontalProps as Props,
-    tRadioHorizontalButtonProps as ButtonProps,
-} from "./types"
