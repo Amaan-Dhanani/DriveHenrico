3a6d5a3ee9ea3de3ec3f8a1ccf0e39e501fd6c1e
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-07-25 00:50:40 -0400

    chore(register): Forgot to change some styling

--- packages/frontend/src/routes/register/_forms/CodeForm.svelte
+++ packages/frontend/src/routes/register/_forms/CodeForm.svelte
@@ -4,6 +4,7 @@
 	import { CodeInput } from '@components';
 	import { Button } from 'sk-clib';
 	import { TextRedactor } from '@components';
+	import { Text } from '@lib/ui';
 
 	const { _state, _user_input, _verification_state } = getRegisterCtx();
 
@@ -44,7 +45,7 @@
 <form class="box-border flex size-full flex-col gap-4" {onsubmit}>
 	<span class="text-center dark:text-white">Verify Code</span>
 
-	<span class="text-center text-sm">We just emailed a code to <TextRedactor text={_user_input.email} className="text-primary"/>. Please check your inbox. If it isn't found, it could be in your spam.</span>
+	<Text class="text-center text-sm">We just emailed a code to <TextRedactor text={_user_input.email} className="text-primary"/>. Please check your inbox. If it isn't found, it could be in your spam.</Text>
 	<CodeInput classWrapper="pb-[3px]" name="code"/>
 
 	<Button type="submit" class="mb-4 cursor-pointer rounded-xl text-white">Continue</Button>
