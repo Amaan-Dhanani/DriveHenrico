be972d0f417aa3448cf7631cf56da9103c1864a9
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-07-26 21:29:45 -0300

    cleanup(backend): Removed unused or annoying routes

--- packages/backend/api/account/delete.py
+++
@@ -1,7 +0,0 @@
-from utils import app
-
-blueprint = app.Blueprint("api:@account#delete", __name__)
-
-@blueprint.route(rule="/account/delete", methods=["POST"])
-async def delete_POST(*args, **kwargs):
-    print("")
