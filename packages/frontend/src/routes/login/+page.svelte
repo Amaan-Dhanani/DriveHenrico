043a2f3776289bcb3e89ea59383b65d2fbaabc5a
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-07-28 13:46:38 -0400

    chore(login): removed onsubmit function b/c it shows email and pwd in console

--- packages/frontend/src/routes/login/+page.svelte
+++ packages/frontend/src/routes/login/+page.svelte
@@ -3,14 +3,6 @@
 	import { Header, Text } from '@ui';
 	import { Button, Flex, Frame } from 'sk-clib';
 	let selected = '';
-
-	async function onsubmit(event: Event) {
-		event.preventDefault(); // Stop default behavior
-
-		const formData = Object.fromEntries(new FormData(event.target as HTMLFormElement));
-
-		console.log('Submitting', formData);
-	}
 </script>
 
 <Flex col fill class="mt-20">
