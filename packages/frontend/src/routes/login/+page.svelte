ac6b1a7f3556d551f55e582f27bb42d89038e29d
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-07-28 16:28:40 -0400

    chore(login): Changed register2 route to login, deleted original login route

--- packages/frontend/src/routes/login/+page.svelte
+++ packages/frontend/src/routes/login/+page.svelte
@@ -1,37 +1,66 @@
 <script lang="ts">
-	import { Input } from '@components';
+	// === Core ===
+	import { Websocket } from '@utils';
+	import { onMount } from 'svelte';
+
+	// === Components ===
 	import { Header, Text } from '@ui';
-	import { Button, Flex, Frame } from 'sk-clib';
-	let selected = '';
+	import { Flex, Frame } from 'sk-clib';
+	import { CodeForm, CredentialForm, Success } from './_forms';
+
+	// === Websocket ===
+	import { auth_login_post, auth_login_confirm_code } from './_ws';
+
+	// === Context ===
+	import { setLoginCtx } from './ctx.svelte';
+	import { Steps } from '@components';
+	import type { StepsHelpers } from '@components/steps/ctx.svelte';
+
+	let ctx = setLoginCtx();
+	const _state = ctx._state;
+
+	let step_helper: StepsHelpers | undefined = $state(undefined);
+
+	const stepTextMap: Record<string, string> = {
+		credential: 'Let\'s hit the road! All you gotta to is log in to DriveHenrico!',
+		code: 'Well, that\'s not all. We also have to verify your information.'
+	};
+
+	$effect(() => {
+		if (!step_helper) return;
+		if (_state.loggedin) {
+			step_helper.ctx.step = step_helper.get_step_idx('done');
+		}
+	});
+
+	onMount(async () => {
+		_state.ws = new Websocket('/auth/login');
+		await _state.ws.connect();
+
+		_state.ws.on('auth:login:post', (error, data) => auth_login_post(ctx, error, data));
+		_state.ws.on('auth:login:confirm_code', (error, data) =>
+			auth_login_confirm_code(ctx, error, data)
+		);
+
+		_state.ws.onerror = (error, data) => {
+			console.error('Oh no... its broken...', error, data);
+		};
+	});
+
+	const steps = [
+		{ name: 'credential', component: CredentialForm },
+		{ name: 'code', component: CodeForm },
+		{ name: 'done', component: Success }
+	];
 </script>
 
 <Flex col fill class="mt-20">
 	<!-- Header -->
 	<Header xxl bold class="ml-4 sm:ml-0">Sign In</Header>
-	<Text lg class="ml-4 opacity-80 sm:ml-0">Welcome back to DriveHenrico!</Text>
+	<Text lg class="ml-4 opacity-80 sm:ml-0">{stepTextMap[_state.step]}</Text>
 
 	<!-- Form Section -->
 	<Frame flex col fill class="mt-2 box-border rounded-t-2xl bg-white p-6 dark:bg-[#2F2F42]">
-		<form class="box-border flex size-full flex-col" {onsubmit}>
-			<Input class="mb-4" type="email" id="email_input" name="email" label="Email" />
-			<Input
-				type="password"
-				class="mb-[12px]"
-				id="password_input"
-				label="Password"
-				name="password"
-			/>
-
-			<Button type="submit" class="mb-4 cursor-pointer rounded-xl text-white">Sign In</Button>
-
-			<Flex row center class="gap-2">
-				<Text lg class="opacity-80">Don't have an account?</Text>
-				<a href="/register" class="text-primary font-bold underline">Sign Up</a>
-			</Flex>
-			<Flex row center class="gap-2">
-				<Text lg class="opacity-80">Forgot Password?</Text>
-				<a href="/forget" class="text-primary font-bold underline">Click here</a>
-			</Flex>
-		</form>
+		<Steps {steps} bind:helper={step_helper} />
 	</Frame>
 </Flex>
