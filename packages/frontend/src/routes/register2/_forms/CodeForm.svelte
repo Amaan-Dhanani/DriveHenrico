c9817ba64aef2bee1c116789890fbd8aede0e665
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-07-29 15:19:48 -0300

    fix(register2): Connection Success

--- packages/frontend/src/routes/register2/_forms/CodeForm.svelte
+++ packages/frontend/src/routes/register2/_forms/CodeForm.svelte
@@ -16,11 +16,12 @@
 	onMount(() => {
 		const { email, password } = _helpers.mergeSteps(
 			'credential',
-			'account_type',
-			'code'
 		);
 
 		_session_initiate_state.setAll({ method: 'email', email, password });
+
+		
+		_state.session_ws?.send("session:initiate", {method: "email", email, password})
 	});
 
 	async function onsubmit(event: Event) {
