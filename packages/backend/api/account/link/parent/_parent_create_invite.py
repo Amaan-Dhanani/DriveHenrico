8ade16f82223b447a3fd581b7f5b8775ae215735
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-08-01 06:28:35 -0300

    feat(link): parent:create_invite logic
    Closes #17

--- packages/backend/api/account/link/parent/_parent_create_invite.py
+++ packages/backend/api/account/link/parent/_parent_create_invite.py
@@ -1,2 +1,46 @@
+# === Core ===
+from dataclasses import dataclass
+
+# === Utilities
+from utils import websocket_auth, websocket_message
+from utils.abc import Invite
+
+# === Typing ===
+from typing import Optional
+
+# === Errors ===
+from utils.exception.websocket import WebsocketException 
+
+@dataclass
+class D:
+    email: Optional[str] = None
+
 async def parent_create_invite(*_, **__):
-    pass
+    """
+    Parent wants to create an invite
+    
+    :param Optional[str] email: Email of the student, if this is 
+    present, also send an email to the student in question so long 
+    as they're a registered student
+    
+    :raises WebsocketException:
+
+        - If the user doesn't exist
+        - If the user isn't a parent account
+    """
+    
+    data: D = websocket_message.cast_data(D)
+    
+    user = websocket_auth.user
+    
+    if user is None:
+        raise WebsocketException(operation="creation:failed", message="User isn't specified, did you auth?")
+    
+    if not user.is_parent:
+        raise WebsocketException(operation="creation:failed", message="You are not a parent")
+    
+    invite = Invite.from_user(user)
+    invite.insert()
+    
+    return "invite:created", {"invite_id": invite.id, "invite_code": invite.code}
+    
