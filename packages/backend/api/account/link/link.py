174c868d221214366d6769ed10b5213e641d1099
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-08-02 16:58:18 -0300

    feat(link): student:link structure

--- packages/backend/api/account/link/link.py
+++ packages/backend/api/account/link/link.py
@@ -4,7 +4,7 @@ from utils.helper.websocket import Websocket
 
 # === Listeners ===
 from .teacher import teacher_create_class, teacher_regenerate_code
-from .student import student_link_class, student_link_parent, student_unlink
+from .student import student_link, student_unlink
 from .parent import parent_create_invite, parent_revoke_invite
 
 blueprint = app.Blueprint("api(account):@link", __name__)
@@ -15,8 +15,7 @@ _ws = Websocket()
 @_ws.auth(global_auth=True)
 @_ws.on("operation", value="teacher:create_class", callback=teacher_create_class)
 @_ws.on("operation", value="teacher:regenerate_code", callback=teacher_regenerate_code)
-@_ws.on("operation", value="student:link_class", callback=student_link_class)
-@_ws.on("operation", value="student:link_parent", callback=student_link_parent)
+@_ws.on("operation", value="student:link", callback=student_link)
 @_ws.on("operation", value="student:unlink", callback=student_unlink)
 @_ws.on("operation", value="parent:create_invite", callback=parent_create_invite)
 @_ws.on("operation", value="parent:revoke_invite", callback=parent_revoke_invite)
