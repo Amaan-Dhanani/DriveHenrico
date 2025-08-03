58567e9fe9d1cb4b200bc5c28262c682344ed0bb
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-08-02 23:57:11 -0300

    feat(invite): Add link method
    Makes the route code cleaner

--- packages/backend/utils/abc/handlers/invite.py
+++ packages/backend/utils/abc/handlers/invite.py
@@ -6,6 +6,7 @@ from pydantic import Field
 from secrets import token_hex
 
 # === Utils ===
+from packages.backend.utils.exception.websocket import WebsocketException
 from utils.helper.time import now, future
 from utils.abc.handlers.user import User
 from utils.abc.handlers._class import Class
@@ -32,8 +33,46 @@ class Invite(WrapperModel):
     __collection__: ClassVar[Collection] = MongoClient.invites
     id: str = Field(default_factory=lambda: token_hex(32))
     
+    def link(self, user: User) -> None:
+        """
+        Links a user to the target of this invite. Depending on what
+        the type of the invite this is, the user will either be linked to a class or
+        a parent
+        
+        :param User user: User that's going to be linked
+        """
+        
+        if not user.is_student:
+            raise WebsocketException(None, "You aren't a student")
+
+        target = self.target
+        
+        if target is None:
+            raise WebsocketException(None, message="No target was found when linking")
+
+        if self.type == "class":
+            if not isinstance(target, Class):
+                raise WebsocketException(None, message="Expected a Class as target")
+            
+            if target.has_student(user.id):
+                raise WebsocketException(None, message="You're already in the class")
+            
+            target.add_student(user.id)
+
+        elif self.type == "parent":
+            if not isinstance(target, User):
+                raise WebsocketException(None, message="Expected a User as parent target")
+            
+            if user.id in (target.student_ids or []):
+                raise WebsocketException(None, message="You're already linked to this parent")
+            
+            target.update(None, "$addToSet", {"student_ids": user.id})
+
+        else:
+            raise WebsocketException(None, message="Unmatched target type")
+
     @property
-    def target(self) -> User | None:
+    def target(self) -> User | Class | None:
         """
         Attempts to get the target attached to this session
         
